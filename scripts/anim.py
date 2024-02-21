e43f77e2e2173e45cfbba77db2f1880b3fd06332
DerChair <ghostik792@gmail.com>
2024-02-21 15:55:29 +0600

    upd

--- scripts/anim.py
+++ scripts/anim.py
@@ -7,14 +7,14 @@ df = pd.read_csv(file_path)
 
 # Создание графика пирога
 fig, ax = plt.subplots(figsize=(12, 8))
-wedges, texts, autotexts = ax.pie(df['Всего правонарушений'], labels=None, autopct='%1.1f%%', startangle=140, wedgeprops=dict(width=0.4), pctdistance=0.85)
+wedges, texts, autotexts = ax.pie(df['Total_crimes'], labels=None, autopct='%1.1f%%', startangle=140, wedgeprops=dict(width=0.4), pctdistance=0.85)
 
 # Установка местоположения текста внутри или снаружи пирога
 ax.axis('equal')  # Убедитесь, что круг выглядит как круг
 plt.title('Распределение правонарушений по регионам в 2022 году')
 
 # Добавление легенды
-legend = ax.legend(df['Регион'], title='Регионы', bbox_to_anchor=(1, 0.5), loc="center right", bbox_transform=plt.gcf().transFigure)
+legend = ax.legend(df['Region'], title='Регионы', bbox_to_anchor=(1, 0.5), loc="center right", bbox_transform=plt.gcf().transFigure)
 
 # Изменение местоположения текста
 for text, autotext in zip(texts, autotexts):
